b37a2f9a0db460d1a740028e56265a99d4aa6a39
JessikaFreire <jessika-freire@hotmail.com>
2022-09-07 18:21:03 -0300

    Section 6

--- Module1IntroductionToJava/Section6Arrays/Videos/TDArrays.java
+++ Module1IntroductionToJava/Section6Arrays/Videos/TDArrays.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class TDArrays {
     public static void main(String[] args) {
         /*
+         * 1
          * int[][] grades = new int[3][4];
          * 
          * grades[0][0] = 72;
@@ -22,18 +23,49 @@ public class TDArrays {
          * grades[2][2] = 99;
          * grades[2][3] = 100;
          */
+        /*
+         * System.out.println("\tHarry: " + Arrays.toString(grades[0]));
+         * System.out.println("\tRon: " + Arrays.toString(grades[1]));
+         * System.out.println("\tHermione: " + Arrays.toString(grades[2]));
+         */
 
         int[][] grades = {
                 { 72, 74, 78, 76 },
                 { 65, 64, 61, 67 },
                 { 95, 98, 99, 100 }
         };
-        //System.out.println("\tHarry: " + Arrays.toString(grades[0]));
-        //System.out.println("\tRon: " + Arrays.toString(grades[1]));
-        //System.out.println("\tHermione: " + Arrays.toString(grades[2]));
 
-        System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
-        System.out.println("\tRon: " + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
-        System.out.println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+        for (int i = 0; i < grades.length; i++) {
+
+            switch (i) {
+                case 0:
+                    System.out.print("\tHarry: ");
+                    break;
+                case 1:
+                    System.out.print("\tRon: ");
+                    break;
+                case 2:
+                    System.out.print("\tHermione: ");
+                    break;
+                default:
+                    break;
+            }
+            for (int j = 0; j < grades[i].length; j++) {
+                System.out.print(grades[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+        /*
+         * 2
+         * System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " +
+         * grades[0][2] + " " + grades[0][3]);
+         * System.out.println("\tRon: " + grades[1][0] + " " + grades[1][1] + " " +
+         * grades[1][2] + " " + grades[1][3]);
+         * System.out
+         * .println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " +
+         * grades[2][2] + " " + grades[2][3]);
+         */
+
     }
 }
